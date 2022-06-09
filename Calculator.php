f8b16da8efa62d29ef3a59f87c3b29149d8d1e7f
liamageo <103076900+liamageo@users.noreply.github.com>
2022-06-09 19:15:35 +0000

    Adds subtract method. Tests all go green!

--- Calculator.php
+++ Calculator.php
@@ -5,12 +5,17 @@ class Calculator
   function add($a, $b) {
 		return $a + $b;
   }
+
+  function subtract($a, $b) {
+		return $a - $b;
+  }
   
   function multiply($a, $b) {
 		return $a * $b;
   }
   
   function divide($a, $b) {
+    if($b==0){return "ERR";}
 		return $a / $b;
   }
 
---
+++ calculator-add-test.php
@@ -0,0 +1,13 @@
+<?php
+//calculator-add-test.php
+require_once 'simpletest/autorun.php';
+include 'Calculator.php';
+
+class TestOfCalculator extends UnitTestCase {
+	function testAdd() {
+	  $myCalc = new Calculator();
+	  $this->assertEqual($myCalc->add(1,1), 2, "1 + 1");
+	  $this->assertEqual($myCalc->add(2,2), 4, "2 + 2");
+	  $this->assertEqual($myCalc->add(1), 1, "three params, 1 + 1 + 1");
+	}
+}
